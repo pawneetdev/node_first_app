f6395e27d04a4ae5a84863cd54f88e302d5cc123
pawneetdev <rubalps@gmail.com>
2021-11-08 18:20:40 +0530

    path helper

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const logger = require('./logger');
 
 console.log(logger); // { log: [Function: log] }
 logger.log('message'); // message
+
+console.log('-----------------');
+
+const files = require('./files');
+console.log(files.path);
